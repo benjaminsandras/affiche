68423a6ef85d41dc92249e3b10cf3f86298c3a7c
benjaminsandras <bensand77@hotmail.fr>
2019-06-15 19:10:21 +0200

    add jquery-ui

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 
 $("#g10015").attr('data-toggle', 'modal').attr('data-target', '#modal_activities').attr('class', 'animated').attr('class', 'pulse')
 
-// $("#g10015").click(function () {
-// 	$("#g10015").css()
-// })
+// $( "#path7689" ).hover(function() {
+// 	$( "#path7689" ).toggle( "scale" , { percent: "90%" },500);
+// 	// scaleUp(this, "200px")
+
+// } )
+
+$( "#path7689" ).hover(function() {
+	$( "#path7689" ).addClass('animate');
+	// scaleUp(this, "200px")
+} )
+
+$( "#path7689" ).mouseout(function() {
+	$( "#path7689" ).removeClass('animate');
+	// scaleUp(this, "200px")
+} )
+
+// $( "#path7689" ).mouseover(function() {
+// 	$( "#path7689" ).toggle( "scale" )
+// } )
+// ​
+// $( "#path7689" ).mouseout(function() {
+// 	$( "#path7689" ).show( "scale")
+// } )
 
 $("#tspan10027").attr('data-toggle', 'modal').attr('data-target', '#modal_activities').attr('class', 'animated').attr('class', 'pulse')
 
