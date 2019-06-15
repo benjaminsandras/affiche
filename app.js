22207b2d041d149da2963e1cae844f111549fec6
benjaminsandras <bensand77@hotmail.fr>
2019-06-15 13:15:53 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+$("#g10015").attr('data-toggle', 'modal').attr('data-target', '#modal_activities').attr('class', 'animated').attr('class', 'pulse')
+
+// $("#g10015").click(function () {
+// 	$("#g10015").css()
+// })
+
+$("#tspan10027").attr('data-toggle', 'modal').attr('data-target', '#modal_activities').attr('class', 'animated').attr('class', 'pulse')
+
+$("#tspan10023").attr('data-toggle', 'modal').attr('data-target', '#modal_activities').attr('class', 'animated').attr('class', 'pulse')
+
